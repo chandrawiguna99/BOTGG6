3b43de14640d25fe6eb5af72cc327a20b2c59990
chandrawiguna99 <chndrwgnn66@gmail.com>
2022-02-05 23:56:49 +0700

    BOTGG6

---
+++ shop/sewa.js
@@ -0,0 +1,23 @@
+const pc_sewa = () => {
+return `「 *PRICE LIST* 」
+							
+⬣ PRICE SEWABOT
+
+⬡ PERMANEN = Rp 20.000
+⬡ PERMANEN + USER PREM = Rp 30.000
+
+
+⬣ PRICE JADIBOT
+
+⬡ JADI BOT PERMANEN 25K
+
+
+⬣ MINAT? PM
+⬡ wa.me/6285861009691
+
+NOTE : 
+Bot On 24 Jam Karna Di Run Menggunakan Heroku
+Reset Data Setiap Jam 02:00 `
+	}
+
+exports.pc_sewa = pc_sewa
